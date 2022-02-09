f4c1478b5911f784cc34503dc0ff8a30e24375d0
Oscar Castro <oscar.a.castro818@gmail.com>
2022-02-09 10:43:10 -0600

    added to array-lecture.js with new exercise

--- js/arrays-lecture.js
+++ js/arrays-lecture.js
@@ -23,21 +23,41 @@
 //
 // }
 
+//
+// var cities = ["San Antonio", "Austin", "Dallas", "Houston"]
+//
+//
+// for (var i = 0; i < cities.length; i++) {
+//     console.log("Student number " + (i+1) + " lives in " + cities[i])
+// }
+//
+// var prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
+//
+//
+// prices.forEach(function(elements, i) {
+//     console.log("Item number " + (i+ 1) + " cost $" +elements);
+// })
+//
 
-var cities = ["San Antonio", "Austin", "Dallas", "Houston"]
 
 
-for (var i = 0; i < cities.length; i++) {
-    console.log("Student number " + (i+1) + " lives in " + cities[i])
-}
+var hamletQuote = "To be or not to be, that is the question";
+var noCommasHamletQuote = hamletQuote.replace(",", "").toLowerCase();
+var hamletQuoteArray = noCommasHamletQuote.split(" ");
 
-var prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
-
-
-prices.forEach(function(elements, i) {
-    console.log("Item number " + (i+ 1) + " cost $" +elements);
-})
+console.log(hamletQuoteArray);
+hamletQuoteArray.reverse();
+console.log(hamletQuoteArray);
 
 
+var hamletQuoteBackwards = hamletQuoteArray.join(" ");
+console.log(hamletQuoteBackwards);
 
+var hamletQuoteWordList = hamletQuoteArray.join(",");
+console.log(hamletQuoteWordList);
 
+var bringBackTheOriginal = hamletQuoteWordList.replaceAll(","," ");
+var theOriginalArray = bringBackTheOriginal.split(" ");
+theOriginalArray.reverse();
+var theOriginalQuote = theOriginalArray.join(" ");
+console.log(theOriginalQuote);
