d9d84b78f6b782a06f20969a3c1cf4720327c7b6
Oscar Castro <oscar.a.castro818@gmail.com>
2022-03-17 16:47:31 -0500

    added missing semi-colons

--- js/weather_map.js
+++ js/weather_map.js
@@ -5,7 +5,8 @@ $(window).on('load', function(){
 
 });
 
-// MAP JAVASCRIPT
+// MAP JAVASCRIPT: MAP, MARKER, AND MARKER DRAGEND
+
 mapboxgl.accessToken = MAPBOX_API_TOKEN;
 var map = new mapboxgl.Map({
     container: 'map',
@@ -13,7 +14,7 @@ var map = new mapboxgl.Map({
     center: [-98.4936, 31.4241],
     zoom: 5
 });
-var marker = new mapboxgl.Marker({draggable: true}) // initial marker location
+var marker = new mapboxgl.Marker({draggable: true})
     .setLngLat([-98.4936, 29.4241])
     .addTo(map);
 
@@ -24,13 +25,16 @@ function onDragEnd() {
 }
 marker.on('dragend', onDragEnd);
 
+
+// SEARCH BAR FUNCTIONALITY
+
 var search = '';
 $('button').click(function (){
     search = $('#search-bar').val();
     searchBar();
 });
 $(document).on('keypress',function(e) {
-    if(e.which == 13) {
+    if(e.which === 13) {
         search = $('#search-bar').val();
         searchBar();
     }
@@ -44,34 +48,38 @@ function searchBar() {
         getLocation();
         map.flyTo({
             center:(search),
-            zoom: 6,
+            zoom: 9,
             speed: 0.5
         });
     });
 }
 
+
+// LOCATION FUNCTIONALITY
+
 function getLocation() {
     var coord = marker.getLngLat();
-    console.log(coord)
+    console.log(coord);
     var lngLat = {lng: coord.lng, lat: coord.lat};
     reverseGeocode2(lngLat, MAPBOX_API_TOKEN).then(function (data) {
         // console.log(data.features[3].place_name)
         var currentLocationData = data.features[3].place_name;
-        var currentLocationArray = currentLocationData.split(' ')
-        console.log(currentLocationArray)
-        currentLocationArray.pop()
-        currentLocationArray.pop()
-        var currentLocationString = currentLocationArray.join(' ')
-        console.log(currentLocationArray)
+        var currentLocationArray = currentLocationData.split(' ');
+        console.log(currentLocationArray);
+        currentLocationArray.pop();
+        currentLocationArray.pop();
+        var currentLocationString = currentLocationArray.join(' ');
+        console.log(currentLocationArray);
         $('#current-location').html(currentLocationString);
     });
 }
 
-// WEATHER JAVASCRIPT
+// WEATHER JAVASCRIPT: 5-DAY FORECAST
+
 var weatherCoordinates = [
         29.4252,
         -98.4916
-]
+];
 function getForecast () {
     $.get('https://api.openweathermap.org/data/2.5/onecall', {
         lat: weatherCoordinates[0],
@@ -87,7 +95,7 @@ function getForecast () {
         function getTemp(e) {
             var max = 'High : ' + '<strong>' + Math.ceil(data.daily[e].temp.max) + '</strong>' + '°';
             var min = 'Low: ' + '<strong>' + Math.ceil(data.daily[e].temp.min) + '</strong>' + '°';
-            return  max + ' / ' + min
+            return  max + ' / ' + min;
         }
         function getDescription(e) {
             return data.daily[e].weather[0].main;
@@ -97,9 +105,9 @@ function getForecast () {
         }
         function getDate(e) {
             var date = new Date(data.daily[e].dt * 1000).toDateString().split(' ');
-            date.pop()
-            console.log(date)
-            return date.join(' ')
+            date.pop();
+            console.log(date);
+            return date.join(' ');
         }
         function getIcon(e) {
             var icon = data.daily[e].weather[0].icon;
@@ -113,11 +121,11 @@ function getForecast () {
         }
         function getSunrise(e) {
             var sunUp = data.daily[e].sunrise * 1000
-            return 'Sunrise: ' + '<strong>'  + new Date(sunUp).toLocaleTimeString("en-US") + '</strong>'
+            return 'Sunrise: ' + '<strong>'  + new Date(sunUp).toLocaleTimeString("en-US") + '</strong>';
         }
         function getSunset(e) {
             var sunDown = data.daily[e].sunset * 1000
-            return 'Sunset: ' + '<strong>'  + new Date(sunDown).toLocaleTimeString("en-US")  + '</strong>'
+            return 'Sunset: ' + '<strong>'  + new Date(sunDown).toLocaleTimeString("en-US")  + '</strong>';
         }
 
             var weather = '';
@@ -134,13 +142,15 @@ function getForecast () {
         $('#forecast').html(weather);
 
 
-        var currentTemp = '<strong>' + Math.ceil(data.current.temp) + '</strong>' +  '°'
-        var currentDescription  = data.current.weather[0].main
-        var icon = data.current.weather[0].icon
+// WEATHER JAVASCRIPT: CURRENT WEATHER
+
+        var currentTemp = '<strong>' + Math.ceil(data.current.temp) + '</strong>' +  '°';
+        var currentDescription  = data.current.weather[0].main;
+        var icon = data.current.weather[0].icon;
         var currentIcon =  '<img src="http://openweathermap.org/img/w/'+ icon +'.png">';
 
         var weather2 = '';
-            weather2 += '<div class="current-weather">' + currentTemp + ' ' + currentDescription + currentIcon + '</div>'
+            weather2 += '<div class="current-weather">' + currentTemp + ' ' + currentDescription + currentIcon + '</div>';
 
         $('#current-weather').html(weather2);
     });
