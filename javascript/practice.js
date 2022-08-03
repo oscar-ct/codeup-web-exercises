85c13881f067c08b4359d2a5a7ac9dbc8b1f0473
Oscar Castro <oscar.a.castro818@gmail.com>
2022-08-02 23:59:07 -0500

    solved code signal js challanges

--- javascript/practice.js
+++ javascript/practice.js
@@ -219,6 +219,26 @@ function solution(n) {
 }
 
 
+// Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+//
+//     Example
+//
+// For statues = [6, 2, 3, 8], the output should be
+// solution(statues) = 3.
+//
+// Ratiorg needs statues of sizes 4, 5 and 7.
+
+function solution(statues) {
+
+    let sortedStatues = statues.sort()
+    let lowNum = sortedStatues[0];
+    let highNum = sortedStatues[statues.length - 1];
+    return ((highNum - lowNum + 1) - (statues.length))
+
+}
+
+
+
 
 
 
