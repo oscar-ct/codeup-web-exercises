209107f2c169d28575b43c7f71870d8536eb4d80
Oscar Castro <oscar.a.castro818@gmail.com>
2022-08-20 23:50:55 -0500

    feat: completed codesignal js challange

--- javascript/practice.js
+++ javascript/practice.js
@@ -849,3 +849,33 @@ function maxSequence2(arr){
     });
     return maxSum;
 }
+
+
+
+// Several people are standing in a row and need to be divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+//
+//     You are given an array of positive integers - the weights of the people. Return an array of two integers, where the first element is the total weight of team 1, and the second element is the total weight of team 2 after the division is complete.
+//
+//     Example
+//
+// For a = [50, 60, 60, 45, 70], the output should be
+// solution(a) = [180, 105].
+
+function solution(a) {
+    let numOne = 0;
+    let numTwo = 0;
+    const numArray = [];
+    for (let i = 0; i < a.length; i++) {
+        if ([i] % 2 === 0) {
+            numOne += a[i];
+        } else {
+            numTwo += a[i];
+        }
+    }
+    numArray.push(numOne);
+    numArray.push(numTwo);
+    console.log(numOne);
+    console.log(numTwo);
+    console.log(numArray);
+    return numArray;
+}
