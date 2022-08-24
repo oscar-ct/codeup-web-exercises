e51573e6901c46dd289c58ea9d042a9f0f53fb09
Oscar Castro <oscar.a.castro818@gmail.com>
2022-08-23 23:56:24 -0500

    feat: completed js challange

--- javascript/practice.js
+++ javascript/practice.js
@@ -946,16 +946,30 @@ function solution(a, b) {
             }
         }
     }
-
     console.log(count)
-
-
-
     if (count === a.length) {
         return true;
     } else {
         return false;
     }
-
-
 }
+
+
+// You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array. =>
+function longestConsec(strarr, k) {
+    if (strarr.length === 0 || k <= 0 || k > strarr.length) {
+        return "";
+    }
+
+    let longStr = "";
+    let newStr = "";
+
+    for (let i = 0; i < strarr.length; i++) {
+        newStr = strarr.slice(i, i + k);
+        if (newStr.join("").length > longStr.length ) {
+            longStr = newStr.join("");
+        }
+    }
+    return longStr;
+}
+// console.log(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2));
