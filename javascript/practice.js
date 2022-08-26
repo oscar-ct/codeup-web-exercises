2063e57c4c2d22c1642fd4b1848a3b7164bcb0ea
Oscar Castro <oscar.a.castro818@gmail.com>
2022-08-25 23:43:31 -0500

    feat: solved js challange

--- javascript/practice.js
+++ javascript/practice.js
@@ -1018,3 +1018,31 @@ function longestConsec(strarr, k) {
     return longStr;
 }
 // console.log(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2));
+
+
+for (let i = 0; i < 1000; i++) {
+    count++
+    console.log(count)
+}
+
+
+// You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+//
+//     Example
+//
+// For inputArray = [1, 1, 1], the output should be
+// solution(inputArray) = 3.
+
+
+function solution(inputArray) {
+    let max = inputArray[0];
+    let moves = 0;
+    for(let i = 1; i < inputArray.length; i++) {
+        if (inputArray[i] <= max) {
+            moves += max - inputArray[i] + 1;
+            inputArray[i] = max + 1;
+        }
+        max = inputArray[i]
+    }
+    return moves
+}
