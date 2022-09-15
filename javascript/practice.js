062e3fc11b557b9bee0afb9516c34124e66e637e
Oscar Castro <oscar.a.castro818@gmail.com>
2022-09-14 23:34:36 -0500

    feat: solved js challanges

--- javascript/practice.js
+++ javascript/practice.js
@@ -1696,3 +1696,66 @@ function extractEachKth(inputArray, k) {
     });
 
 }
+
+
+
+// Find the leftmost digit that occurs in a given string.
+//
+//     Example
+//
+// For inputString = "var_1__Int", the output should be
+// solution(inputString) = '1';
+// For inputString = "q2q-q", the output should be
+// solution(inputString) = '2';
+// For inputString = "0ss", the output should be
+// solution(inputString) = '0'.
+
+
+
+function firstDigit(inputString) {
+    const inputStrArr = inputString.split('');
+    let numStr = '';
+    inputStrArr.forEach(function(char) {
+        if (!isNaN(parseInt(char))) {
+            numStr += char;
+        }
+    })
+
+    console.log(numStr[0]);
+    return numStr[0];
+}
+
+//
+// Given a string, find the number of different characters in it.
+//
+//     Example
+//
+// For s = "cabca", the output should be
+// solution(s) = 3.
+//
+// There are 3 different characters a, b and c.
+//
+//     Input/Output
+//
+//     [execution time limit] 4 seconds (js)
+//
+//     [input] string s
+//
+// A string of lowercase English letters.
+//
+//     Guaranteed constraints:
+//     3 ≤ s.length ≤ 1000.
+//
+//     [output] integer
+
+
+function differentSymbolsNaive(s) {
+    const strArr = s.split('');
+    let diffChar = '';
+    strArr.forEach(function(char) {
+        if (!diffChar.includes(char)) {
+            diffChar += char;
+        }
+    });
+    return diffChar.length;
+}
