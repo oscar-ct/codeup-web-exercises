678910054d5d875fe821963a03383b61a9118a25
Oscar Castro <oscar.a.castro818@gmail.com>
2022-10-12 21:36:44 -0500

    feat: solved circle of numbers js challange

--- javascript/practice.js
+++ javascript/practice.js
@@ -2128,3 +2128,24 @@ function sumUpNumbers(inputString) {
 
 
 }
+
+
+
+// Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+//
+// Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+//
+//     Example
+//
+// For n = 10 and firstNumber = 2, the output should be
+// solution(n, firstNumber) = 7.
+
+
+function CircleOfNumbers(n, firstNumber) {
+
+    console.log(n);
+    console.log(firstNumber);
+
+    return (n/2 + firstNumber) % n;
+
+}
